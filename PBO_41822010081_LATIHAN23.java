e8a5510de47aac50a34460929b3d7cc857311175
Anastasya Azami <80338392+Tasyul@users.noreply.github.com>
2023-09-28 20:20:37 +0700

    Add files via upload

--- PBO_41822010081_LATIHAN23.java
+++ PBO_41822010081_LATIHAN23.java
@@ -1,6 +1,12 @@
 package TASYA;
 import java.util.Scanner;
-
+/**
+ * @author nstsy
+ * Nama : Anastasya azami
+ * NIM : 41822010081
+ * Nama : Syadella Putri Ardana
+ * NIM: 41822010037
+ */
 public class PBO_41822010081_LATIHAN23 {
     public static void main(String[] args) {
         System.out.println("======= Program Nilai Terbesar dan Terkecil Nilai Mahasiswa ========");
